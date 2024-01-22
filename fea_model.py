73d579fc00f3c576be716c90debf4be378799c3d
Zoha Peterson <zohapeterson@gmail.com>
2024-01-22 16:12:58 -0500

    having trouble with some matrix functionality

--- fea_model.py
+++ fea_model.py
@@ -5,12 +5,12 @@ from math import *
 input_nodes = input_elasticMod = input_length = input_height = input_crossSection = input_force = input_depth = input_dia = None
 window_gap = 50
 
-# Receive input for relevant properties, set height and width of the window
+# Receive input for relevant properties
 #while (not input_elasticMod.isdigit()):
 input_elasticMod = 100000#input("Enter the elastic modulus [Pa]: ")
 
 #while (not input_length.isdigit()):
-input_length = 500#input("Enter the width of the model [m]: ")
+input_length = 500#input("Enter the length of the model [m]: ")
 
 #while(not input_height.isdigit()):
 input_height = 200#input("Enter the height of the model [m]: ")
@@ -30,15 +30,15 @@ input_force = 100#input("Enter the force acted upon the beam [N]: ")
 #while(cross_section.toUpper() != "RECTANGULAR" or cross_section.toUpper() != "CIRCULAR"):
 input_crossSection = "Rectangular"#input("Enter the cross section of the beam [Rectangular or Circular]: ")
 
-print("Your model has "  + str(input_nodes) + " nodes, an elastic modulus of " + str(input_elasticMod) + "Pa, a width of " + str(input_length) + "m, and a height of " + str(input_height) + "m.")
+# print("Your model has "  + str(input_nodes) + " nodes, an elastic modulus of " + str(input_elasticMod) + "Pa, a length of " + str(input_length) + "m, and a height of " + str(input_height) + "m.")
 
 width = 2 * window_gap + input_length
 height = 2 * window_gap + input_height
 
 # Create Object class
 class Model:
-    def __init__(self, model_width, model_height, model_elasticMod, node_division, cross_section, force):
-        self.model_width = model_width
+    def __init__(self, model_length, model_height, model_elasticMod, node_division, cross_section, force):
+        self.model_length = model_length
         self.model_height = model_height
         self.model_elasticMod = model_elasticMod
         self.nodes = node_division
@@ -46,9 +46,10 @@ class Model:
         self.force = force
         self.BC_dim = 10
         self.modules = []
-
+        self.entire_kMatrix = []
 
         self.createForce()
+        self.createKmatrix()
     
     def createForce(self):
         if(self.force > 0):
@@ -62,39 +63,49 @@ class Model:
 
     def createNodes(self):
         for i in range(0, self.nodes):
-            self.modules.append(Module(input_elasticMod, input_crossSection, input_depth, input_height, input_length, input_dia, input_nodes))
-            #self.modules.append(canvas.create_rectangle((window_gap + (i * (self.model_width / self.nodes))), window_gap, ((window_gap + (i * (self.model_width / self.nodes))) + (self.model_width / self.nodes)), (window_gap + self.model_height)))
-
+            self.modules.append(Module(input_elasticMod, input_crossSection, input_depth, input_height, input_length, input_dia, input_nodes, i))
         self.createFixedSupport()
 
     def createFixedSupport(self):
-        canvas.create_rectangle(window_gap + self.model_width, window_gap,window_gap + self.model_width + 10, window_gap + self.model_height, fill="black")
+        canvas.create_rectangle(window_gap + self.model_length, window_gap,window_gap + self.model_length + 10, window_gap + self.model_height, fill="black")
+
+    def createKmatrix(self):
+        for this_node in self.modules:
+            self.entire_kMatrix.append(this_node.ind_kMatrix)
+        
+        print(self.entire_kMatrix)
 
 class Module:
-    def __init__(self, elastic_mod, cross_section, depth, height, length, dia, numNodes):
+    def __init__(self, elastic_mod, cross_section, depth, height, length, dia, numNodes, index):
         self.elastic_mod = elastic_mod
         self.mod_crossSection = cross_section
         self.depth = depth
         self.height = height
+        self.length = length
         self.dia = dia
+        self.numNodes = numNodes
         self.modLength = self.length / self.numNodes
         self.area = 0
         self.k_value = 0
-        self.k_matrix = []
+        self.draw_module = canvas.create_rectangle((window_gap + (index * (self.length / self.numNodes))), window_gap, ((window_gap + (index * (self.length / self.numNodes))) + (self.length / self.numNodes)), (window_gap + self.height))
+        self.ind_kMatrix = []
+
+        self.calcArea()
+        self.calcK()
+        self.setKMatrix()
     
     def calcArea(self):
-        if(self.mod_crossSection.toUpper() == "RECTANGULAR"):
+        if(self.mod_crossSection.upper() == "RECTANGULAR"):
             self.area = self.depth * self.height
-        elif(self.mod_crossSection.toUpper() == "CIRCULAR"):
+        elif(self.mod_crossSection.upper() == "CIRCULAR"):
             self.area = pi * (self.dia / 2) ** 2
 
     def calcK(self):
         self.k_value = (self.elastic_mod * self.area) / self.modLength
 
     def setKMatrix(self):
-        self.k_matrix.append([[self.k_value, -self.k_value],[-self.k_value, self.k_value]])
-        
-
+        self.ind_kMatrix.append([[self.k_value, -self.k_value],[-self.k_value, self.k_value]])
+        print(self.ind_kMatrix)
 
 ## Create GUI -- window and canvas
 window = Tk()
